3fbf2261a6f61d78456bbfa023c7c30455cd8c84
Glen Mazza <gmazza@talend.com>
2012-05-17 19:05:29 -0400

    Adding cxf_x509_profile (interceptor and ws-secpol versions) to blog samples

---
+++ cxf_x509_profile_secpol/client/src/main/java/client/ClientKeystorePasswordCallback.java
@@ -0,0 +1,34 @@
+package client;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.ws.security.WSPasswordCallback;
+
+public class ClientKeystorePasswordCallback implements CallbackHandler {
+    
+    private Map<String, String> passwords = 
+        new HashMap<String, String>();
+    
+    public ClientKeystorePasswordCallback() {
+        passwords.put("myclientkey", "ckpass");
+    }
+
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        for (int i = 0; i < callbacks.length; i++) {
+            WSPasswordCallback pc = (WSPasswordCallback)callbacks[i];
+
+            String pass = passwords.get(pc.getIdentifier());
+            if (pass != null) {
+                pc.setPassword(pass);
+                return;
+            }
+        }
+    }   
+}
+
