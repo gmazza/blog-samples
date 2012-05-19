0aa8b32ef2e42e4f377192098e2ff0616fba6ce1
Glen Mazza <gmazza@talend.com>
2012-05-18 22:14:40 -0400

    Added metro X509 security tutorial

---
+++ metro_x509_profile/client/src/main/java/client/WSClient.java
@@ -0,0 +1,22 @@
+package client;
+
+import org.example.contract.doubleit.DoubleItPortType;
+import org.example.contract.doubleit.DoubleItService;
+
+public class WSClient {
+    public static void main (String[] args) {
+        DoubleItService service = new DoubleItService();
+        DoubleItPortType port = service.getDoubleItPort();           
+
+        doubleIt(port, 10);
+        doubleIt(port, 0);
+        doubleIt(port, -10);
+    } 
+    
+    public static void doubleIt(DoubleItPortType port, 
+            int numToDouble) {
+        int resp = port.doubleIt(numToDouble);
+        System.out.println("The number " + numToDouble + " doubled is " 
+            + resp);
+    }
+}
