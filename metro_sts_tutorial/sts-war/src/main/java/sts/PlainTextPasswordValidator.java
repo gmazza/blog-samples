b9a9552ae9cb6a98d4de608db72d14ee7f45d14e
gmazza <glen.mazza@gmail.com>
2012-05-24 11:49:44 -0400

    Added Metro STS tutorial to source downloads.

---
+++ metro_sts_tutorial/sts-war/src/main/java/sts/PlainTextPasswordValidator.java
@@ -0,0 +1,23 @@
+package sts;
+
+import com.sun.xml.wss.impl.callback.PasswordValidationCallback;
+
+public class PlainTextPasswordValidator implements
+        PasswordValidationCallback.PasswordValidator {
+
+    public boolean validate(PasswordValidationCallback.Request request)
+            throws PasswordValidationCallback.PasswordValidationException {
+
+        PasswordValidationCallback.PlainTextPasswordRequest plainTextRequest 
+            = (PasswordValidationCallback.PlainTextPasswordRequest) request;
+        if ("alice".equals(plainTextRequest.getUsername())
+                && "clarinet".equals(plainTextRequest.getPassword())) {
+            return true;
+        } else if ("bob".equals(plainTextRequest.getUsername())
+                && "trombone".equals(plainTextRequest.getPassword())) {
+            return true;
+        }
+        return false;
+    }
+}
+
